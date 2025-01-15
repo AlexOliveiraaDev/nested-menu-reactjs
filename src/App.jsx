e093c24f738b68df9d1c820cc86d6cc443906a05
Alex Oliveira <contatoalexandergv@gmail.com>
2025-01-15 20:59:13 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import NestedMenu from "./components/nested-menu/NestedMenu.jsx";
+
+function App() {
+  return (
+    <div>
+      <NestedMenu />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nested-menu/NestedMenu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NestedMenu.css";
+
+const NestedMenu = () => {
+  return (
+    <div>
+      <h1>Teste</h1>
+    </div>
+  );
+};
+
+export default NestedMenu;
