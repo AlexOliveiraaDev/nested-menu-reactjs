ff7ebb6fb8b62e2d5b5d9ced73b24fa877751b0c
Alex Oliveira <contatoalexandergv@gmail.com>
2025-01-18 19:25:00 -0300

    fix(UI): Fixed validateDOMNesting error on Header

--- src/components/nested-menu/nestedmenu-header/NestedMenuHeader.jsx
+++ src/components/nested-menu/nestedmenu-header/NestedMenuHeader.jsx
@@ -2,35 +2,32 @@ import React, { useState } from "react";
 import { Plus, ChevronDown, ChevronUp } from "lucide-react";
 import "./NestedMenuHeader.css";
 
-const NestedMenuHeader = ({onHeaderClick, onAddClick}) => {
+const NestedMenuHeader = ({ onHeaderClick, onAddClick }) => {
   const [open, switchOpen] = useState(false);
-  
+
   // Send a callback to NestedMenu and flip the header state
   const handleClick = () => {
     switchOpen(!open);
     onHeaderClick();
   };
 
-
-
-
   return (
     <div className="main-header-div">
-      <button className="header-button" onClick={handleClick}>
+      <div className="header-button" onClick={handleClick}>
         {open ? <ChevronDown /> : <ChevronUp />}
         <span>Layers</span>
-        
+
         {/*The StopPropagation prevents header-button from being clicked */}
         <button
           className="header-add-button"
           onClick={(e) => {
             e.stopPropagation();
-            onAddClick()
+            onAddClick();
           }}
         >
           <Plus />
         </button>
-      </button>
+      </div>
     </div>
   );
 };
