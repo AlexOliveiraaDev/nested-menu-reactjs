900eb1ecb7a58092840578da3455ced21632999d
Alex Oliveira <contatoalexandergv@gmail.com>
2025-01-19 20:47:19 -0300

    fix(vite): fixed missing index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// export type definition files
-import dts from "vite-plugin-dts";
 
-import path from "path";
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: "src" })],
-  build: {
-    // build package as a library
-    lib: {
-      entry: path.resolve(__dirname, "src/main.tsx"),
-      // es = ES module
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      // avoid bundling react and react-dom
-      // as they will be already required and bundled by the user app
-      // using this application
-      external: ["react", "react-dom", "react/jsx-runtime", "tailwindcss"],
-    },
-    sourcemap: true,
-  },
+  plugins: [react()],
 });
